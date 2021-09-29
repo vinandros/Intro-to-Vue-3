7238a6397fb8dd31685f2a64c3d66a49beb0f166
vinandros <vinandros@gmail.com>
2021-09-28 21:35:41 -0600

    Binding v-model

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      url: "https://www.vinandros.com",
+    };
+  },
+});
