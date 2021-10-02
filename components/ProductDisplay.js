3ca00d14dfd9fe05ae4180b6c4023640a681e9b0
vinandros <vinandros@gmail.com>
2021-10-01 21:49:29 -0600

    v-model | two-way data binding

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,13 +1,13 @@
-app.component('product-display', {
+app.component("product-display", {
   props: {
     premium: {
       type: Boolean,
-      required: true
-    }
+      required: true,
+    },
   },
-  template: 
-  /*html*/
-  `<div class="product-display">
+  template:
+    /*html*/
+    `<div class="product-display">
     <div class="product-container">
       <div class="product-image">
         <img v-bind:src="image">
@@ -41,42 +41,58 @@ app.component('product-display', {
 
       </div>
     </div>
+    <review-list v-show="reviews.length > 0" :reviews="reviews"></review-list>
+    <review-form @review-submitted="addReview"></review-form>
   </div>`,
   data() {
     return {
-        product: 'Socks',
-        brand: 'Vue Mastery',
-        selectedVariant: 0,
-        details: ['50% cotton', '30% wool', '20% polyester'],
-        variants: [
-          { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-        ]
-    }
+      product: "Socks",
+      brand: "Vue Mastery",
+      selectedVariant: 0,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
+      ],
+      reviews: [],
+    };
   },
   methods: {
-      addToCart() {
-          this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
-      },
-      updateVariant(index) {
-          this.selectedVariant = index
-      }
+    addToCart() {
+      this.$emit("add-to-cart", this.variants[this.selectedVariant].id);
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+    addReview(review) {
+      this.reviews.push(review);
+    },
   },
   computed: {
-      title() {
-          return this.brand + ' ' + this.product
-      },
-      image() {
-          return this.variants[this.selectedVariant].image
-      },
-      inStock() {
-          return this.variants[this.selectedVariant].quantity
-      },
-      shipping() {
-        if (this.premium) {
-          return 'Free'
-        }
-        return 2.99
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    shipping() {
+      if (this.premium) {
+        return "Free";
       }
-  }
-})
+      return 2.99;
+    },
+  },
+});
---
+++ components/ReviewForm.js
@@ -0,0 +1,58 @@
+app.component("review-form", {
+  template:
+    /*html*/
+    `<form class="review-form" @submit.prevent="onSubmit">
+        <h3>Leave a review</h3>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name">
+
+        <label for="recommendation"> Would you recommend this product? </label>
+        <input id="recommendation" type="text" v-model="recommendation">
+
+        <label for="review">Review:</label>      
+        <textarea id="review" v-model="review"></textarea>
+
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+        <option>5</option>
+        <option>4</option>
+        <option>3</option>
+        <option>2</option>
+        <option>1</option>
+        </select>
+        
+
+        <input class="button" type="submit" value="Submit">
+    </form>`,
+  data() {
+    return {
+      name: "",
+      review: "",
+      rating: null,
+      recommendation: "",
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (
+        this.name === "" ||
+        this.review === "" ||
+        this.rating === null ||
+        this.recommendation === ""
+      ) {
+        alert("Review is incomplete. Please fill out every field.");
+        return;
+      }
+      let productReview = {
+        name: this.name,
+        review: this.review,
+        rating: this.rating,
+        recommendation: this.recommendation,
+      };
+      this.$emit("review-submitted", productReview);
+      this.name = "";
+      this.review = "";
+      this.rating = null;
+    },
+  },
+});
